b0b2f0e8b12219ec1a25eff59354cc8ea7f76f4b
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-06-22 20:08:34 -0300

    configured setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import reportWebVitals from './misc/reportWebVitals';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './misc/index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +17,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if(process.env.NODE_ENV !== 'production') reportWebVitals(console.log);

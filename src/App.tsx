faf94bde0fd9efda36f35511cf858fbd71d661f8
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-06-22 20:30:42 -0300

    dashboard structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-
+import { AppBar, Box, Container, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import theme from './misc/theme';
 
 export default function App() {
 
-  return <></>;
+    return (
+        
+        <ThemeProvider theme={ theme }>
+
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Desafio Imersão Full Cycle
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
+            <Container sx={{ marginY: 3 }}>
+                
+            </Container>
+
+        </ThemeProvider>
+
+    );
 
 }
 
